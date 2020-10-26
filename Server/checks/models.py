c9aeef2d4522d7a22df0a265d6887d80988b6a41
10k20 <10k20.dev@gmail.com>
2020-10-26 20:57:37 +0300

    add check types to models

--- Server/checks/models.py
+++ Server/checks/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
+def list_check_types():
+    check_types_list=[(u'kitchen',u'kitchen'),
+                        (u'client',u'client') ]
+    return check_types_list
+
 class Printer(models.Model):
     name = models.CharField(max_length=120)
     api_key = models.CharField(max_length=120)
-    check_type = models.CharField(max_length=120)
+    check_type = models.CharField(max_length=120, choices=list_check_types())
     point_id = models.IntegerField()
 
     def __str__(self):
